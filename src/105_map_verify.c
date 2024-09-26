561f88ca6d42f1fefbd658cff7091914d43a85ae
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2024-09-26 21:29:44 +0100

    to help with zappa's ocd :gift:

--- src/105_map_verify.c
+++ src/105_map_verify.c
@@ -13,25 +13,44 @@
 #include "../inc/cub3d.h"
 #include <fcntl.h>
 
-int	ft_flood_fill(t_map *map, t_map *aux_map, int x, int y)
-{
-	char	*c;
+int	ft_verify_borders(t_map *map);
+int	ft_verify_paths(t_map *map);
+int	ft_flood_fill(t_map *map, t_map *aux_map, int x, int y);
 
-	if (map == NULL || aux_map == NULL)
+t_map	*ft_map_verify(t_map *map)
+{
+	if (map == NULL)
+		return (NULL);
+	if (map->ceiling_color == -1 || map->floor_color == -1)
+		return (ft_parse_err(PARSE_COLORS), ft_map_free(map), NULL);
+	if (map->start_direction == INVALID || map->start_pos == NULL)
+		return (ft_parse_err(PARSE_DIR), ft_map_free(map), NULL);
+	if (ft_verify_paths(map))
+		return (ft_parse_err(PARSE_PATH), ft_map_free(map), NULL);
+	if (ft_verify_borders(map))
+		return (ft_parse_err(PARSE_BORD), ft_map_free(map), NULL);
+	return (map);
+}
+
+int	ft_verify_paths(t_map *map)
+{
+	int	i;
+	int	fd;
+
+	if (map == NULL)
 		return (FAILURE);
-	c = ft_map_at_i_ref(aux_map, x, y);
-	if (c == NULL)
-		return (FAILURE);
-	if (*c == 0)
-		return (SUCCESS);
-	// missing verify if '0' is not on border or near to space
-	if (ft_flood_fill(map, aux_map, x - 1, y))
-		return (FAILURE);
-	if (ft_flood_fill(map, aux_map, x + 1, y))
-		return (FAILURE);
-	if (ft_flood_fill(map, aux_map, x, y - 1))
-		return (FAILURE);
-	return (ft_flood_fill(map, aux_map, x, y + 1));
+	i = NORTH;
+	while (i < EAST)
+	{
+		if (map->paths[i] == NULL)
+			return (FAILURE);
+		fd = open(map->paths[i], O_RDONLY);
+		if (fd < 0)
+			return (FAILURE);
+		close(fd);
+		++i;
+	}
+	return (SUCCESS);
 }
 
 int	ft_verify_borders(t_map *map)
@@ -57,38 +76,23 @@ int	ft_verify_borders(t_map *map)
 	return (ft_free_arr(aux_map.map), i);
 }
 
-int	ft_verify_paths(t_map *map)
+int	ft_flood_fill(t_map *map, t_map *aux_map, int x, int y)
 {
-	int	i;
-	int	fd;
+	char	*c;
 
-	if (map == NULL)
+	if (map == NULL || aux_map == NULL)
 		return (FAILURE);
-	i = NORTH;
-	while (i < EAST)
-	{
-		if (map->paths[i] == NULL)
-			return (FAILURE);
-		fd = open(map->paths[i], O_RDONLY);
-		if (fd < 0)
-			return (FAILURE);
-		close(fd);
-		++i;
-	}
-	return (SUCCESS);
-}
-
-t_map	*ft_map_verify(t_map *map)
-{
-	if (map == NULL)
-		return (NULL);
-	if (map->ceiling_color == -1 || map->floor_color == -1)
-		return (ft_parse_err(PARSE_COLORS), ft_map_free(map), NULL);
-	if (map->start_direction == INVALID || map->start_pos == NULL)
-		return (ft_parse_err(PARSE_DIR), ft_map_free(map), NULL);
-	if (ft_verify_paths(map))
-		return (ft_parse_err(PARSE_PATH), ft_map_free(map), NULL);
-	if (ft_verify_borders(map))
-		return (ft_parse_err(PARSE_BORD), ft_map_free(map), NULL);
-	return (map);
+	c = ft_map_at_i_ref(aux_map, x, y);
+	if (c == NULL)
+		return (FAILURE);
+	if (*c == 0)
+		return (SUCCESS);
+	// missing verify if '0' is not on border or near to space
+	if (ft_flood_fill(map, aux_map, x - 1, y))
+		return (FAILURE);
+	if (ft_flood_fill(map, aux_map, x + 1, y))
+		return (FAILURE);
+	if (ft_flood_fill(map, aux_map, x, y - 1))
+		return (FAILURE);
+	return (ft_flood_fill(map, aux_map, x, y + 1));
 }
